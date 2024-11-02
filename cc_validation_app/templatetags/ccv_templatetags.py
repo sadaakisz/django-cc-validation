d7c0fedceaaf62957e411337af68517671d8ea84
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-11-02 12:25:08 -0500

    Refactored buttons to components.

--- cc_validation_app/templatetags/ccv_templatetags.py
+++ cc_validation_app/templatetags/ccv_templatetags.py
@@ -27,3 +27,40 @@ def index_card(context):
     return {
         "card": context["card"],
     }
+
+@register.inclusion_tag(components_dir + "main_button.html", takes_context=True)
+def main_button(context, icon, text, text_color, href_url, position="", size="", bg_color="", rounded=""):
+    request = context.get("request")
+    if not position: position = "fixed mx-auto inset-x-0 bottom-8"
+    if not size: size = "h-16 w-40"
+    if not bg_color: bg_color = "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+    if not rounded: rounded = "rounded-full"
+    return {
+        "icon": icon,
+        "text": text,
+        "text_color": text_color,
+        "href_url": href_url,
+        "position": position,
+        "size": size,
+        "bg_color": bg_color,
+        "rounded": rounded,
+    }
+
+@register.inclusion_tag(components_dir + "main_button_with_pk.html", takes_context=True)
+def main_button_with_pk(context, icon, text, text_color, href_url, pk, position="", size="", bg_color="", rounded=""):
+    request = context.get("request")
+    if not position: position = "fixed mx-auto inset-x-0 bottom-8"
+    if not size: size = "h-16 w-40"
+    if not bg_color: bg_color = "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+    if not rounded: rounded = "rounded-full"
+    return {
+        "icon": icon,
+        "text": text,
+        "text_color": text_color,
+        "href_url": href_url,
+        "pk": pk,
+        "position": position,
+        "size": size,
+        "bg_color": bg_color,
+        "rounded": rounded,
+    }
