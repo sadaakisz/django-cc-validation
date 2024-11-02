2ac4206db3ec254cc26f82239ae926f7b0c2d620
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2024-11-02 11:10:46 -0500

    Made header href dynamic.

--- cc_validation_app/templatetags/ccv_templatetags.py
+++ cc_validation_app/templatetags/ccv_templatetags.py
@@ -13,11 +13,12 @@ def head(context, title, context_id=""):
     }
 
 @register.inclusion_tag(components_dir + "header.html", takes_context=True)
-def header(context, top_left_icon):
+def header(context, top_left_icon, href_url="cc_validation_app:index"):
     request = context.get("request")
 
     return {
         "top_left_icon": top_left_icon,
+        "href_url": href_url,
     }
 
 @register.inclusion_tag(components_dir + "index_card.html", takes_context=True)
